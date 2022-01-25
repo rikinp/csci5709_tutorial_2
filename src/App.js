0118ec450021469ace01c22bb59d1d718d1e57fa
rikinp <rikin201@gmail.com>
2022-01-24 20:53:01 -0400

    1st commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> This is the Rikin Patel's sample React starter page for CSCI 5709 Tutorial-2!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
